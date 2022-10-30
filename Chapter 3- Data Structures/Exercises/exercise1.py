549daae8dcd71548c8dfb1c6f100fd5b17f8f93a
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-10-30 15:57:13 +0400

    Create exercise1.py

---
+++ Chapter 3- Data Structures/Exercises/exercise1.py
@@ -0,0 +1,7 @@
+#Exercise 1 :  Names
+# Store the names of a few of your friends in a list called names. 
+# Print each person’s name by accessing each element in the list, 
+# one at a time.
+
+name = [ "Rice", "Cess", "Hermosa", "Chicken", "Dzy", "Apirl", "Hulia"]
+print(name, "\n")
