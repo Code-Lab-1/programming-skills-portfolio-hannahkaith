6f82b6c9b929ab530b510b9dbfc8c0e8ded05914
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-10-30 15:57:00 +0400

    Create exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -0,0 +1,14 @@
+#Exercise 3 : Stripping Numbers 
+name = "     Ike\t"
+print (name, "Eveland" "\n")
+
+stripped_name = name.lstrip()
+print(stripped_name, "Eveland" "\n")
+
+stripped_name = name.rstrip()
+print(stripped_name, "Eveland" "\n")
+
+stripped_name = name.strip()
+print(stripped_name, "Eveland" "\n")
+
+
