4d5716d1d44f98bf5fbb9e53cb3dac89c2a0f897
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:15:02 +0400

    Create exercise8(extra).py

---
+++ Chapter 7- Functions/Exercises/exercise8(extra).py
@@ -0,0 +1,10 @@
+# Exercise 8 (Class Activity)
+def ave():
+  a = int(input("Enter first number: "))
+  b = int(input("Enter second number: "))
+  c = int(input("Enter third number: "))
+  d = int(input("Enter fourth number: "))
+
+  average = (a + b + c + d)/4
+  print(average)
+ave()
