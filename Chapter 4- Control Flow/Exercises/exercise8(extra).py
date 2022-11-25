f554168a63d0c7c3086a629acda8e6610482bb6f
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:11:19 +0400

    Create exercise8(extra).py

---
+++ Chapter 4- Control Flow/Exercises/exercise8(extra).py
@@ -0,0 +1,13 @@
+#Exercise 8
+s = int(input("Type a number: "))
+k = int(input("Type a number: "))
+
+print(" The result of " + str(s) + " == " + str(k) + " is -> " + str(s==k) )
+print(" The result of " + str(s) + " != " + str(s) + " is -> " + str(s!=k) )
+print(" The result of " + str(s) + " > " + str(s) + " is -> " + str(s>k) )
+print(" The result of " + str(s) + " < " + str(s) + " is -> " + str(s<k) )
+print(" The result of " + str(s) + " <= " + str(s) + " is -> " + str(s<=k) )
+print(" The result of " + str(s) + " >= " + str(s) + " is -> " + str(s>=k) )
+
+
+
