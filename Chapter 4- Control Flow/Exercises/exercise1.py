934726011f38bcec46e5029cfa7279f2c1b4ad6d
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:10:53 +0400

    Create exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,8 @@
+# Exercise 1 : Alien Colors #1
+alien_color = 'green'
+if alien_color == 'green':
+    print('You\'ve gained 5 points :> \n')
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You\'ve gained 5 points :>')
