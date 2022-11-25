a0daff23131e646b35360a68cf74621d553b58c5
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:09:24 +0400

    Create exercise8(extra).py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise8(extra).py
@@ -0,0 +1,16 @@
+#Exercise 8
+x = 47
+print (x)
+x += 87
+print (x, "\n")
+
+x = 20
+print (x)
+x -=45
+print (x)
+
+age = 15
+weight = 115.0 
+height = 158.7 
+print("She is a " + str(age) + " year old that weighs " + str(weight) + " lb. and is " + str(height) + " cm tall!")
+
