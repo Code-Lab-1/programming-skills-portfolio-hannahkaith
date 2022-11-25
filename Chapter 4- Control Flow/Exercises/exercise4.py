d71589acf6e9783ec67fa2bd543a1b5ac2029a9a
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:11:07 +0400

    Create exercise4.py

---
+++ Chapter 4- Control Flow/Exercises/exercise4.py
@@ -0,0 +1,14 @@
+# Exercise 4 : Stages of life
+age = 69
+if age < 2:
+    print('You\'re just a baby uwu')
+elif age < 4:
+    print('You\'re a toddler awe')
+elif age < 13:
+    print('You\'re a kid ew')
+elif age < 20:
+    print('You\'re a teenager sad')
+elif age < 65:
+    print('You\'re an adult oof')
+else:
+    print('You\'re a fossil- an elder :p')
