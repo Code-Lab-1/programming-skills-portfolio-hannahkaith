0db9f13c580a27fdb77827bd114f3b498e3a605c
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:14:49 +0400

    Create exercise2.py

---
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -0,0 +1,5 @@
+# Exercise 2: Favorite Book 
+def favorite_book(title = " 'The Traveling Cat Chronicles' "):
+    print("My all-time favorite book is called" + title + " <3")
+    print("\n")
+favorite_book()
