28142674ecc846a2800a5aaa29a97160644c55d7
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:14:51 +0400

    Create exercise3.py

---
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -0,0 +1,8 @@
+#Exercise 3: T-Shirt  
+def make_shirt(size, text):
+  print("\n")
+  print("Let's make a t-shirt in a " + size + " size!")
+  print('The text on the shirt will say, "' + text + '" ♡')
+
+make_shirt('small', 'You can fly even higher')
+make_shirt(text = "Now go, and don't look back", size = 'large')
