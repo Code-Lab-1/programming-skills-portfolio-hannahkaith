6d5527754199916231cdd05da1dd2345a38e8d32
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:11:30 +0400

    Create exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise2.py
@@ -0,0 +1,28 @@
+# Exercise 2: Glossary
+glossary = {
+    "Floats" : " -> used to represent numbers that aren’t integers / whole numbers ✻ ",
+    "Python" : " -> high-level programming language, with applications in numerous areas ✻ ",
+    "Append Method" : " -> adds an item to the end of an existing list ✻ ",
+    "Print" : " -> used to output text ✻ ",
+    "List" : " -> used to store items ✻ "
+}
+
+term = 'Floats'
+print('\n')
+print(term + ": " + "\n" + glossary[term])
+
+term = 'Python'
+print('\n')
+print(term + ": " + "\n" + glossary[term])
+
+term = 'Append Method'
+print('\n')
+print(term + ": " + "\n" + glossary[term])
+
+term = 'Print'
+print('\n')
+print(term + ": " + "\n" + glossary[term])
+
+term = 'List'
+print('\n')
+print(term + ": " + "\n" + glossary[term])
