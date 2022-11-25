f5618081b35f56c3464fbfa67026fb473fcf9a64
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:42:41 +0400

    Update exercise8(extra).py

--- Chapter 1- Getting Started with Python/Exercises/exercise8(extra).py
+++ Chapter 1- Getting Started with Python/Exercises/exercise8(extra).py
@@ -9,8 +9,3 @@ print (x)
 x -=45
 print (x)
 
-age = 15
-weight = 115.0 
-height = 158.7 
-print("She is a " + str(age) + " year old that weighs " + str(weight) + " lb. and is " + str(height) + " cm tall!")
-
