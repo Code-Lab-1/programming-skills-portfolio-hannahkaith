a39b594c3ef1b893fcfc017a12b424715daaef04
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:11:01 +0400

    Create exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,27 @@
+# Exercise 3 : Alien Colors #3
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You\'ve gained 5 points :>')
+elif alien_color == 'yellow':
+    print('You\'ve gained 10 points :D')
+else:
+    print('You\'ve gained 15 points! Nice one!')
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('You\'ve gained 5 points :>')
+elif alien_color == 'yellow':
+    print('You\'ve gained 10 points :D')
+else:
+    print('You\'ve gained 15 points! Nice one!')
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('You\'ve gained 5 points :>')
+elif alien_color == 'yellow':
+    print('You\'ve gained 10 points :D')
+else:
+    print('You\'ve gained 15 points! Nice one!')
