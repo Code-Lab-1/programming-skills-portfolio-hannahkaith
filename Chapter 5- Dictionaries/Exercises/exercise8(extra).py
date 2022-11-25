984f8efb00985c91e900c7996b7f40575b94f1d7
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:13:47 +0400

    Create exercise8(extra).py

---
+++ Chapter 5- Dictionaries/Exercises/exercise8(extra).py
@@ -0,0 +1,12 @@
+#Exercise 8
+species = {
+    "bird": "house sparrow", 
+    "cat": "persian"
+    }
+print("1.", species["cat"])
+
+species["butterfly"] = "monarch butterfly"
+print("2.", species)
+
+species.pop("bird")
+print("3.", species)
