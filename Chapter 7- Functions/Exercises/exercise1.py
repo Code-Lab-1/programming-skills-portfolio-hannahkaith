3feee456d9d0b4280e1b9d5497e3d3a87b72f7ec
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:43:13 +0400

    Update exercise1.py

--- Chapter 7- Functions/Exercises/exercise1.py
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -1,5 +1,5 @@
 # Exercise 1:   Message
 def display_message():
-    print("This chapter, I am learnig all about FUNCTIONS, how to create one, and its purpose!")
+    print("This chapter, I am learning all about FUNCTIONS, how to create one, and its purpose!")
     print("\n")
 display_message()
