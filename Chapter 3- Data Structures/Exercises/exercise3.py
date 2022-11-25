755cec816e40d20805666b30670fc872b4390169
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:10:30 +0400

    Update exercise3.py

--- Chapter 3- Data Structures/Exercises/exercise3.py
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -1,1 +1,7 @@
 #Exercise 3 : Your Own List
+transpo = ["off-road SUVs", "Venice Simplon-Orient-Express", "convertible porsche", "Dubai metro", "Haeundae sky capsule"]
+print("My dream car are " + transpo[0] + ".")
+print("The " + transpo[1] + " is a luxurious way to sightsee different European cities.")
+print("A " + transpo[2] + " is the perfect way to experience the summer breeze.")
+print("The " + transpo[3] +  " is both fast and convenient to go to places.")
+print(transpo[4] + " is a cozy method to appreciate the Busan sea.")
