20d3f183d831d8d83d0c5c82bfdf2983cf906c5a
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:10:10 +0400

    Create exercise6(extra).py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise6(extra).py
@@ -0,0 +1,7 @@
+#Exercise 6 (Class Activity)
+num_1 = int(input('Enter your 1st number -> '))
+num_2 = int(input('Enter your 2nd number -> '))
+num_3 = int(input('Enter your 3rd number -> '))
+
+output = (num_1 + num_2) * num_3
+print(output)
