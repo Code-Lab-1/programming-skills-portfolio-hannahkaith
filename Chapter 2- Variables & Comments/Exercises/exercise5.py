068cbaccad1ec9bb77450d46fd69113e5aff9da7
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2022-11-25 21:09:57 +0400

    Update exercise5.py

--- Chapter 2- Variables & Comments/Exercises/exercise5.py
+++ Chapter 2- Variables & Comments/Exercises/exercise5.py
@@ -1,11 +1,10 @@
 #Exercise 5 : USB Shopper
-#A girl heads to a computer shop to buy some USB sticks. 
-# She loves USB sticks and wants as many as she can get for £50. They are £6 each.
-#Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
-#You will to use the arithmetic operators to complete this exercise.
-
 money = 50
 price = 6
 
+usb_sticks = int(money / price)
+print("She'll receive {0} USB sticks.".format(usb_sticks))
 
+change = money - (usb_sticks * price)
+print("Her change will be {0}.".format(change))
 
